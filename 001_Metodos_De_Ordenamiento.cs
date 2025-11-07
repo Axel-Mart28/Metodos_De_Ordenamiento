118996bd397a1999ff7539eb1ee368b4c265a1cd
Axel-Mart28 <axelxdramos10@gmail.com>
2025-11-07 01:51:40 +0000

    cambio1

---
+++ 001_Metodos_De_Ordenamiento.cs
@@ -0,0 +1,6 @@
+//¿Que significa ordenamiento? Es la operación de arreglar los elementos en algún orden secuencial de acuerdo a un criterio de
+//ordenamiento. El propósito principal de un ordenamiento es el de facilitar las búsquedas de los miembros del
+//conjunto ordenado.
+
+// Conviene usar éstos métodos cuando se requiere hacer una cantidad considerable de búsquedas y es importante el factor tiempo.
+//Existen 2 tipos de ordenamientos: Internos y Externos
